d39cb666878370f405462ac48645a355c5ea492e
stive1213 <estifanosamsalu833@gmail.com>
2025-03-22 01:08:50 -0700

    `Refactor App.js: Remove unused imports and update dependencies`

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import Player from "./components/PlayerSong";
+import Song from "./components/Song";
+import "./styles/app.css";
+import data from "./data";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+function App (){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
